580df44ae8b4bc2954abb39d1087176a9436b389
yasirnurprasetyo <yasirnurprasetyo@gmail.com>
2023-11-01 13:50:21 +0700

    add script

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -7,38 +7,60 @@
 	<title>K-24 | <?= $header ?></title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- jQuery -->
+	<script src="<?= base_url(); ?>assets/plugins/jquery/jquery.min.js"></script>
+	<!-- jQuery UI 1.11.4 -->
+	<script src="<?= base_url(); ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
 	<!-- Font Awesome -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
 	<!-- Ionicons -->
 	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 	<!-- Tempusdominus Bbootstrap 4 -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
 	<!-- iCheck -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
 	<!-- JQVMap -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/jqvmap/jqvmap.min.css">
-	<!-- DataTables -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
-	<!--select2 css-->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/select2/css/select2.min.css">
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/jqvmap/jqvmap.min.css">
 	<!-- Theme style -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>dist/css/adminlte.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/dist/css/adminlte.min.css">
 	<!-- overlayScrollbars -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
 	<!-- Daterange picker -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/daterangepicker/daterangepicker.css">
 	<!-- summernote -->
-	<link rel="stylesheet" href="<?= base_url() . 'assets/' ?>plugins/summernote/summernote-bs4.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/summernote/summernote-bs4.css">
 	<!-- Google Font: Source Sans Pro -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-	<!-- jQuery -->
-	<script src="<?= base_url() . 'assets/' ?>plugins/jquery/jquery.min.js"></script>
-	<!-- jQuery UI 1.11.4 -->
-	<script src="<?= base_url() . 'assets/' ?>plugins/jquery-ui/jquery-ui.min.js"></script>
-	<script>
-		window.base_url = '<?= base_url() ?>';
-	</script>
+	<script src="https://kit.fontawesome.com/b0cb772f4e.js" crossorigin="anonymous"></script>
+	<!-- Select2 -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/select2/css/select2.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+	<!-- jquery-validation -->
+	<script src="<?= base_url() . 'assets/' ?>plugins/jquery-validation/jquery.validate.js"></script>
+	<script src="<?= base_url() . 'assets/' ?>plugins/jquery-validation/additional-methods.js"></script>
+	<script src="<?= base_url() . 'assets/' ?>plugins/jquery-validation/localization/messages_id.js"></script>
+	<!-- Sweet Alert -->
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+	<!-- DataTables -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
+	<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> -->
+	<style type="text/css">
+		img.tengah {
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+		}
+
+		h3.text-app {
+			margin-top: 0px;
+			font-size: 20px;
+			text-align: center;
+		}
+	</style>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -161,6 +183,22 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
+
+	<!-- bs-custom-file-input -->
+	<script src="<?= base_url(); ?>assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+	<!-- Tambahan Ranus -->
+	<script src="<?= base_url(); ?>vendor/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/buttons/js/dataTables.buttons.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/buttons/js/buttons.bootstrap4.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/jszip/jszip.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/pdfmake/pdfmake.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/pdfmake/vfs_fonts.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/buttons/js/buttons.html5.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/buttons/js/buttons.print.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/buttons/js/buttons.colVis.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/responsive/js/dataTables.responsive.min.js"></script>
+	<script src="<?= base_url(); ?>vendor/datatables/responsive/js/responsive.bootstrap4.min.js"></script>
 </body>
 
 </html>
